c329372f25a8b11588ee9f1ae56122d95bc2809f
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-10-05 17:24:16 -0500

    Added wide and color props to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ buttonName }) {
+function Button({ buttonName, buttonColor, buttonWide }) {
+  const styles = (wide, color) => {
+    const wideValue = (wide) ? '50%' : '25%';
+    return ({
+      width: wideValue,
+      backgroundColor: color,
+    });
+  };
+
   return (
-    <button type="submit">{ buttonName }</button>
+    <button style={styles(buttonWide, buttonColor)} type="submit">{ buttonName }</button>
   );
 }
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
+  buttonColor: PropTypes.string,
+  buttonWide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  buttonColor: '#F5913E',
+  buttonWide: false,
 };
 
 export default Button;
