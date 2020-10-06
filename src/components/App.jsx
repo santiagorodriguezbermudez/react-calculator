e73e47c02d0108b73db76e8c5be16f0d89c8b1ae
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-10-06 14:45:44 -0500

    Refactor function into class and add handle click method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,30 @@ import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import Calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="app">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const newData = Calculate(this.state, buttonName);
+    this.setState(newData);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
