62b8ac05c509716aa6b4e192e9c1d7675b378221
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-10-06 11:17:59 -0500

    Implemented calculate main module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,2 +1,38 @@
-import Big from 'big.js';
+import Operate from './operate';
 
+const Calculate = ((calculatorObject, buttonName) => {
+  let { total, next, operation } = calculatorObject;
+
+  switch (buttonName) {
+    case 'A/C':
+      total = null;
+      next = null;
+      operation = null;
+      break;
+    case '+/-':
+      total = total ? total * -1 : total;
+      next = next ? next * -1 : next;
+      break;
+    case (/[x,+,÷,\-,%]/.test(buttonName)):
+      // Use operate
+      break;
+    case '.':
+      next = next ? next + buttonName : buttonName;
+      break;
+    case '=':
+      total = total ? 0 : total;
+      next = null;
+      operation = null;
+      break;
+    default:
+      next = next ? next + buttonName : buttonName;
+  }
+
+  return {
+    total,
+    next,
+    operation,
+  };
+})();
+
+export default Calculate;
