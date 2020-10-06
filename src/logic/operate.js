67e8ddad3ce244777df8a952972d89f4c247a239
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-10-06 11:30:37 -0500

    Implemented Operate module

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,2 +1,36 @@
 import Big from 'big.js';
 
+const Operate = ((numberOne, numberTwo, operation) => {
+  const bigOne = Big(numberOne);
+  const bigTwo = Big(numberTwo);
+  let result;
+
+  switch (operation) {
+    case '+':
+      result = bigOne.plus(bigTwo);
+      break;
+    case '-':
+      result = bigOne.minus(bigTwo);
+      break;
+    case 'x':
+      result = bigOne.mul(bigTwo);
+      break;
+    case '÷':
+      result = numberTwo === '0' ? 'undefined' : numberOne.div(numberTwo);
+      break;
+    case '%':
+      result = numberOne.div(100);
+      break;
+    case '+/-':
+      result = numberOne.mul(-1);
+      break;
+    default:
+      result = 'undefined';
+  }
+
+  return {
+    result,
+  };
+})();
+
+export default Operate;
